4f62b779c21c475410bb966d6c27df78630e770f
skidhi <dextrophysicalmegademolisher@gmail.com>
2025-12-08 22:11:37 -0200

    Update threads.js

--- src/threads.js
+++ src/threads.js
@@ -5095,6 +5095,9 @@ Process.prototype.reportVariadicSum = function (numbers) {
     return this.reportListAggregation(numbers, 'reportSum');
 };
 
+Process.prototype.returnbubble = function () {var bubble = this.talkBubble(
+); return ((bubble instanceof SpeechBubbleMorph) ? bubble.data : '');}; /* Get contents. :) */
+
 Process.prototype.reportSum = function (a, b) {
     return this.hyper(this.reportBasicSum, a, b);
 };
